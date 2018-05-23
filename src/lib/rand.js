1bd3250a93c09ca9c78ca3deb12168c40957d615
risq <contact@valentinledrapier.com>
2018-05-24 00:11:26 +0200

    More random tools

--- src/lib/rand.js
+++ src/lib/rand.js
@@ -1,21 +1,27 @@
-export function sign(value = 1) {
-  return Math.random() < 0.5 ? value : -value;
+// Uniform distribution
+export function float() {
+  return Math.random();
 }
 
-export function bool(percentage = 0.5) {
-  return Math.random() < percentage;
-}
-
-export function spread(value = 1) {
-  return value / 2 - Math.random() * value;
+export function spread(amplitude = 1, initial = 0) {
+  return initial + (amplitude / 2 - float() * amplitude);
 }
 
 export function between(min = 0, max = 1) {
-  return Math.random() * (min - max) + max;
+  return float() * (min - max) + max;
 }
 
 export function betweenInt(min = 0, max = 1) {
   return Math.floor(between(min, max));
+}
+
+// Generic helpers (uniform distribution)
+export function sign(value = 1) {
+  return float() < 0.5 ? value : -value;
+}
+
+export function bool(percentage = 0.5) {
+  return float() < percentage;
 }
 
 export function angle(max = 360) {
@@ -26,34 +32,70 @@ export function oneIn(n) {
   return between(0, n) < 1;
 }
 
+// Triangular distribution
 export function triang() {
-  return 1 - Math.sqrt(Math.random());
+  return 1 - Math.sqrt(float());
 }
 
-export function triangSpread(value = 1) {
-  return value / 2 - triang() * value;
+export function spreadTriang(amplitude = 1, initial = 0) {
+  return initial + (amplitude / 2 - triang() * amplitude);
 }
 
-export function triangBetween(min = 0, max = 1) {
+export function betweenTriang(min = 0, max = 1) {
   return triang * (min - max) + max;
 }
 
-export function triangBetweenInt(min = 0, max = 1) {
-  return Math.floor(triangBetween(min, max));
+export function betweenIntTriang(min = 0, max = 1) {
+  return Math.floor(betweenTriang(min, max));
 }
 
+// Inversed triangular distribution
+export function triangInv() {
+  return Math.sqrt(float());
+}
+
+export function spreadTriangInv(amplitude = 1, initial = 0) {
+  return initial + (amplitude / 2 - triangInv() * amplitude);
+}
+
+export function betweenTriangInv(min = 0, max = 1) {
+  return triangInv * (min - max) + max;
+}
+
+export function betweenIntTriangInv(min = 0, max = 1) {
+  return Math.floor(betweenTriangInv(min, max));
+}
+
+// Exponential distribution
 export function exp() {
-  return Math.random() ** 2;
+  return float() ** 2;
 }
 
-export function expSpread(value = 1) {
-  return value / 2 - triang() * value;
+export function spreadExp(amplitude = 1, initial = 0) {
+  return initial + (amplitude / 2 - exp() * amplitude);
 }
 
-export function expBetween(min = 0, max = 1) {
+export function betweenExp(min = 0, max = 1) {
   return exp * (min - max) + max;
 }
 
-export function expBetweenInt(min = 0, max = 1) {
-  return Math.floor(expBetween(min, max));
+export function betweenIntExp(min = 0, max = 1) {
+  return Math.floor(betweenExp(min, max));
+}
+
+// Inversed exponential distribution
+export function expInv() {
+  return 1 - float() ** 2;
+}
+
+export function spreadExpInv(amplitude = 1, initial = 0) {
+  return initial + (amplitude / 2 - expInv() * amplitude);
+}
+
+export function betweenExpInv(min = 0, max = 1) {
+  return expInv * (min - max) + max;
+}
+
+export function betweenIntExpInv(min = 0, max = 1) {
+  return Math.floor(betweenExpInv(min, max));
 }
