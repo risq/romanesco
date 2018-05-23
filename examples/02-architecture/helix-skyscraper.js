6f2dc8765e6d10b792a77988b826e2f109dcaa38
risq <contact@valentinledrapier.com>
2018-05-24 00:46:50 +0200

    Improve `helix-skyscraper` example

--- examples/02-architecture/helix-skyscraper.js
+++ examples/02-architecture/helix-skyscraper.js
@@ -1,19 +1,36 @@
-rule("floor", function () {
-    const factor = rand.between(0.9, 1.1);
+rule("skyscraper", function () {
+    this.call("floors", {
+        s: 0.5,
+        color: rand.color({ lightness: 0.5 }),
+    });
+});
 
-    this.repeat(rand.between(3, 20), {
-        y: 1.2,
-        ry: rand.between(1, 5),
-        sx: factor,
-        sz: factor,
-        lightness: 1 / factor,
-    }, function () {
-        this.box({ sx: 10, sz: 10 });
-    }, "floor");
+rule("floors", function () {
+    // Return a random value between -0.25 and 0.25,
+    // using "inversed" triangular distribution
+    const factor = rand.spreadTriangInv(0.25);
+
+    // Repeat transform between 5 and 20 times
+    this.repeat(
+        rand.between(5, 20),
+        {
+            y: 1.2,
+            ry: rand.between(1, 5),
+            sx: 1 + factor,
+            sz: 1 + factor,
+            lightness: 1 - factor / 2, // soften the factor
+        },
+        function () {
+            this.box({ sx: 25, sz: 25 });
+        },
+        // When done, recursively call "floors" rule again
+        "floors"
+    );
 }).maxDepth(5);
 
-start(function () {
-    this.call("floor", { s: 0.5, color: "orange" });
-}, () => {
-    viewer.focusCamera();
-});
+start(
+    function () {
+        this.call("skyscraper");
+    },
+    () => viewer.focusCamera()
+);
